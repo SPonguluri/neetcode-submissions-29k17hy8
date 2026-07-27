c8763da45c4038c9adf953d3484590b71f0ab0dc
Sujit Ponguluri <pongulur@usc.edu>
2026-07-27 15:39:30 -0700

    Add: sql-arrays - submission-0

---
+++ SQL/sql-arrays/submission-0.sql
@@ -0,0 +1,19 @@
+create table orders(
+id Integer primary key,
+items Text[],
+total_price Integer
+);
+
+
+
+
+
+
+
+-- Do not modify below this line --
+INSERT INTO orders (id, items, total_price) 
+    VALUES (1, ARRAY['apple', 'banana'], 100),
+          (2, ARRAY['orange', 'grape'], 200),
+          (3, ARRAY['watermelon', 'pineapple'], 300);
+
+SELECT * FROM orders;
